7cc13468b6cc497aa563053146855a663cee1f01
wallsmanBR <alves_thiagobastos@hotmail.com>
2017-07-26 20:43:40 -0300

    atualização na classe KNN

--- src/beans/KNN.java
+++ src/beans/KNN.java
@@ -1,7 +1,10 @@
 package beans;
 
-public class KNN {
+import java.util.ArrayList;
 
+public class KNN {
+	private ArrayList<Imagem> Imagens;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
