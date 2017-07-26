8473a04abfe3a332a74df67f12741f07163f5e29
wallsmanBR <alves_thiagobastos@hotmail.com>
2017-07-26 20:37:24 -0300

    mudanças na classe imagem

--- src/beans/Imagem.java
+++ src/beans/Imagem.java
@@ -1,5 +1,29 @@
 package beans;
 
 public class Imagem {
+	private int [] histograma;
+	private String classe;
+	
+	public Imagem (int [] hist, String classe) {
+		this.histograma = hist;
+		this.classe = classe;
+	}
+
+	public int[] getHistograma() {
+		return histograma;
+	}
+
+	public void setHistograma(int[] histograma) {
+		this.histograma = histograma;
+	}
+
+	public String getClasse() {
+		return classe;
+	}
+
+	public void setClasse(String classe) {
+		this.classe = classe;
+	}
+	
 	
 }
