e054b1a0796f4aef1078124e96b8af7274baf17e
wallsmanBR <alves_thiagobastos@hotmail.com>
2017-07-27 20:34:12 -0300

    correcao de erro na divisao das imagens entre treinamento e teste

--- src/beans/KNN.java
+++ src/beans/KNN.java
@@ -13,31 +13,8 @@ public class KNN {
 	public KNN (Imagem[] imagens){
 		this.imagens = imagens;
 	}
-
-	public Imagem[] getImagens() {
-		return imagens;
-	}
-
-	public void setImagens(Imagem[] imagens) {
-		this.imagens = imagens;
-	}
-
-	public Imagem[] getImagensTreinamento() {
-		return imagensTreinamento;
-	}
-
-	public void setImagensTreinamento(Imagem[] imagens) {
-		this.imagensTreinamento = imagens;
-	}
-
-	public Imagem[] getImagensTest() {
-		return imagensTest;
-	}
-
-	public void setImagensTest(Imagem[] imagens) {
-		this.imagensTest = imagens;
-	}
-
+	
+	
 	
 	public static Imagem[] LerArquivo() throws NumberFormatException, IOException {
 		FileReader file = new FileReader("artefatos\\histogramas e classes.txt");
@@ -66,81 +43,161 @@ public class KNN {
 	public void dividirImagens (Imagem[] imgs){
 		Imagem [] treino = new Imagem[5296];
 		Imagem [] test = new Imagem[2648];
+		int indTreino = 0;
+		int IndTest = 0;
 		
 		for(int aviaoTreino=0 ; aviaoTreino < 545 ; aviaoTreino++){
-			treino[aviaoTreino] = imgs[aviaoTreino];
+			treino[indTreino] = imgs[aviaoTreino];
+			indTreino++;
 		}
+		
 		for(int aviaoTest=545 ; aviaoTest < 817 ; aviaoTest++){
-			test[aviaoTest] = imgs[aviaoTest];
+			imgs[aviaoTest].setClasse(null);
+			test[IndTest] = imgs[aviaoTest];
+			IndTest++;
 		}
+		
 		for(int carroTreino=817 ; carroTreino < 1417 ; carroTreino++){
-			treino[carroTreino] = imgs[carroTreino];
+			treino[indTreino] = imgs[carroTreino];
+			indTreino++;
 		}
+		
 		for(int carroTest=1417 ; carroTest < 1717 ; carroTest++){
-			test[carroTest] = imgs[carroTest];
+			imgs[carroTest].setClasse(null);
+			test[IndTest] = imgs[carroTest];
+			IndTest++;
 		}
+		
 		for(int passaroTreino=1717 ; passaroTreino < 2312 ; passaroTreino++){
-			treino[passaroTreino] = imgs[passaroTreino];
+			treino[indTreino] = imgs[passaroTreino];
+			indTreino++;
 		}
+		
 		for(int passaroTest = 2312 ; passaroTest< 2610 ; passaroTest++){
-			test[passaroTest] = imgs [passaroTest];
+			imgs[passaroTest].setClasse(null);
+			test[IndTest] = imgs [passaroTest];
+			IndTest++;
 		}
+		
 		for (int gatoTreino = 2610 ; gatoTreino < 3089 ; gatoTreino++){
-			treino[gatoTreino] = imgs[gatoTreino];
+			treino[indTreino] = imgs[gatoTreino];
+			indTreino++;
 		}
+		
 		for(int gatoTest = 3089 ; gatoTest < 3329 ; gatoTest++){
-			test[gatoTest] = imgs[gatoTest];
+			imgs[gatoTest].setClasse(null);
+			test[IndTest] = imgs[gatoTest];
+			IndTest++;
 		}
+
 		for (int veadoTreino = 3329 ; veadoTreino < 3797 ; veadoTreino++){
-			treino[veadoTreino] = imgs[veadoTreino];
+			treino[indTreino] = imgs[veadoTreino];
+			indTreino++;
 		}
+
 		for (int veadoTest = 3797 ; veadoTest < 4031 ; veadoTest++){
-			test[veadoTest] = imgs[veadoTest];
+			imgs[veadoTest].setClasse(null);
+			test[IndTest] = imgs[veadoTest];
+			IndTest++;
 		}
+
 		for(int cachorroTreino = 4031 ; cachorroTreino < 4536 ; cachorroTreino++){
-			treino[cachorroTreino] = imgs[cachorroTreino];
+			treino[indTreino] = imgs[cachorroTreino];
+			indTreino++;
 		}
+
 		for (int cachorroTest = 4536 ; cachorroTest < 4788 ; cachorroTest++){
-			test[cachorroTest] = imgs[cachorroTest];
+			imgs[cachorroTest].setClasse(null);
+			test[IndTest] = imgs[cachorroTest];
+			IndTest++;
 		}
+
 		for (int sapoTreino = 4788 ; sapoTreino < 5326 ; sapoTreino++){
-			treino[sapoTreino] = imgs[sapoTreino];
+			treino[indTreino] = imgs[sapoTreino];
+			indTreino++;
 		}
+
 		for (int sapoTest = 5326 ; sapoTest < 5595 ; sapoTest++){
-			test[sapoTest] = imgs[sapoTest];
+			imgs[sapoTest].setClasse(null);
+			test[IndTest] = imgs[sapoTest];
+			IndTest++;
 		}
+
 		for (int cavaloTreino = 5595 ; cavaloTreino < 6040 ; cavaloTreino++){
-			treino[cavaloTreino] = imgs[cavaloTreino];
+			treino[indTreino] = imgs[cavaloTreino];
+			indTreino++;
 		}
+
 		for (int cavaloTest = 6040 ; cavaloTest < 6262 ; cavaloTest++){
-			test[cavaloTest] = imgs[cavaloTest];
+			imgs[cavaloTest].setClasse(null);
+			test[IndTest] = imgs[cavaloTest];
+			IndTest++;
 		}
+
 		for (int navioTreino = 6262 ; navioTreino < 6830 ; navioTreino++){
-			treino[navioTreino] = imgs[navioTreino];
+			treino[indTreino] = imgs[navioTreino];
+			indTreino++;
 		}
+
 		for (int navioTest = 6830 ; navioTest < 7114 ; navioTest++){
-			test[navioTest] = imgs[navioTest];
+			imgs[navioTest].setClasse(null);
+			test[IndTest] = imgs[navioTest];
+			IndTest++;
 		}
+
 		for (int caminhaoTreino = 7114 ; caminhaoTreino < 7667 ; caminhaoTreino++){
-			treino[caminhaoTreino] = imgs[caminhaoTreino];
+			treino[indTreino] = imgs[caminhaoTreino];
+			indTreino++;
 		}
+
 		for (int caminhaoTest = 7667 ; caminhaoTest < 7944 ; caminhaoTest++){
-			test[caminhaoTest] = imgs[caminhaoTest];
+			imgs[caminhaoTest].setClasse(null);
+			test[IndTest] = imgs[caminhaoTest];
+			IndTest++;
 		}
+		
+		this.setImagensTreinamento(treino);
+		this.setImagensTest(test);
+		
+		//PERCENTUAL USADO PARA O TREINO FOI DE 2/3 E PARA O TESTE FOI DE 1/3, DAS IMAGENS
+		//545(treino) 272(test)- aviao (817)
+		//600(treino) 300(test)- carro (900)
+		//595(treino) 298(test)- passaro (893) 
+		//479(treino) 240(test)- gato (719)
+		//468(treino) 234(test)- veado (702)
+		//505(treino) 252(test)- cachorro (757)
+		//538(treino) 269(test)- sapo (807) 
+		//445(treino) 222(test)- cavalo (667) 
+		//568(treino) 284(test)- navio (852) 
+		//553(treino) 277(test)- caminhao (830)
 	}
+	
+	public Imagem[] getImagens() {
+		return imagens;
+	}
+
+	public void setImagens(Imagem[] imagens) {
+		this.imagens = imagens;
+	}
+
+	public Imagem[] getImagensTreinamento() {
+		return imagensTreinamento;
+	}
+
+	public void setImagensTreinamento(Imagem[] imagens) {
+		this.imagensTreinamento = imagens;
+	}
+
+	public Imagem[] getImagensTest() {
+		return imagensTest;
+	}
+
+	public void setImagensTest(Imagem[] imagens) {
+		this.imagensTest = imagens;
+	}
+
 }
 
-//545(treino) 272(test)- aviao (817) ok
-//600(treino) 300(test)- carro (900) ok
-//595(treino) 298(test)- passaro (893) ok 
-//479(treino) 240(test)- gato (719) ok
 
-//468(treino) 234(test)- veado (702) ok
-
-//505(treino) 252(test)- cachorro (757) ok
-//538(treino) 269(test)- sapo (807) ok 
-//445(treino) 222(test)- cavalo (667) ok 
-//568(treino) 284(test)- navio (852) ok 
-//553(treino) 277(test)- caminhao (830)
 
 
--- src/beans/Principal.java
+++ src/beans/Principal.java
@@ -3,6 +3,8 @@ package beans;
 import java.io.IOException;
 import java.util.Random;
 
+import javax.swing.plaf.synth.SynthSeparatorUI;
+
 public class Principal {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
@@ -10,7 +12,19 @@ public class Principal {
 		KNN knn = new KNN (imgs);
 		knn.dividirImagens(imgs);
 		
+		for(int i = 0; i < knn.getImagensTreinamento().length; i++) {
+			System.out.println(knn.getImagensTreinamento()[i].getClasse());
+		}
 		
+		
+		for (int i = 0; i < knn.getImagensTest().length; i++) {
+			if(!(knn.getImagensTest()[i].getClasse() == null)){
+				System.out.print("tem classe:");
+				System.out.println(knn.getImagensTest()[i].getClasse());
+			}
+		}
+		
+		System.out.println("done");
 		
 	}
 }
